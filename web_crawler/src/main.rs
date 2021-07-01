7224a027e15f50a8258acf8d3f494421393dfa48
Paul Tuck <pau1tuck@outlook.com>
2021-07-01 14:22:10 +0800

    "2021-07-01T14:22CST"

--- web_crawler/src/main.rs
+++ web_crawler/src/main.rs
@@ -1,13 +1,20 @@
 use std::io::Read;
 use reqwest::blocking::Client;
+use select::document::Document;
+use select::predicate::Name;
 
 fn main() {
     let client = Client::new();
-    let origin = "https://rolisz.ro/";
+    let origin = "https://www.bbc.com/news/world-europe-57666389";
     let mut response = client.get(origin).send().unwrap();
     println!("Status for {}: {}", origin, response.status());
 
     let mut body = String::new();
     response.read_to_string(&mut body).unwrap();
-    println!("HTML: {}", &body[0..40]);
+
+    // println!("HTML: {}", &body[0..40]);
+    Document::from(body.as_str())
+        .find(Name("a"))
+        .filter_map(|n| n.attr("href"))
+        .for_each(|x| println!("{}", x));
 }
